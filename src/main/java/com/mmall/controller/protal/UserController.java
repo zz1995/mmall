aee3e777048eacbd72b6593d58387447a1761c4f
zz1995 <1245697330@qq.com>
2017-06-06 11:21:22 +0800

    Rejected commit: 1.1

--- src/main/java/com/mmall/controller/protal/UserController.java
+++ src/main/java/com/mmall/controller/protal/UserController.java
@@ -1,10 +1,10 @@
-package com.mmall.controller.portal;
+package com.mmall.controller.protal;
 
 import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,8 +18,10 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
+
     private IUserService iUserService;
+
+
     /*
     *登录
     * */
@@ -34,8 +36,9 @@ public class UserController {
         }
         return response;
     }
+
     //登出
-    @RequestMapping(value="logout.do",method = RequestMethod.GET)
+    @RequestMapping(value="logout.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> logout(HttpSession session){
         session.removeAttribute(Const.CURRENT_USER);
@@ -84,4 +87,42 @@ public class UserController {
     public ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken){
         return iUserService.forgetResetPassword(username,passwordNew,forgetToken);
     }
+
+    @RequestMapping(value="reset_password.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> resetPassword(HttpSession session,String passwordOld,String passwordNew){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        return iUserService.resetPassword(passwordOld,passwordNew,user);
+    }
+
+    @RequestMapping(value = "update_information.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> updateInformation(HttpSession session,User user){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        user.setId(currentUser.getId());
+        user.setUsername(currentUser.getUsername());
+        ServerResponse<User> response = iUserService.updateInformation(user);
+        if(response.isSuccess()){
+            response.getData().setUsername(currentUser.getUsername());
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "get_information.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getInformation(HttpSession session){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.createByErrorMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,需要登录");
+        }
+        return iUserService.getInformation(currentUser.getId());
+    }
+
 }
--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -27,4 +27,8 @@ public interface UserMapper {
     int checkAnswer(@Param("username")String username,@Param("question")String question,@Param("answer")String answer);
 
     int updatePasswordByUsername(@Param("username")String username,@Param("password")String password);
+
+    int checkPassword(@Param("password")String password,@Param("userId")Integer userId);
+
+    int checkEmailByUserId(@Param(value="email")String email,@Param(value="userId")Integer userId);
 }
